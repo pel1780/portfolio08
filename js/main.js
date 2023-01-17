1697312245bba0c7f498affafa0ab71cc368c4ef
pel1780 <118698633+pel1780@users.noreply.github.com>
2023-01-17 17:41:22 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -5,5 +5,71 @@ $(function () {
         $('h1').toggleClass('on');
     });
 
-    $('.main_product_slide').slick({});
+    $('.header .right a').on('click', function () {
+        $('.header .gnb').toggleClass('on');
+        $('.header .right a').toggleClass('on');
+    });
+
+
+    var lastScrollTop = 0, delta = 5;
+
+    $(window).on('scroll', function () {
+        var sct = $(window).scrollTop();
+
+        // if (Math.abs(lastScrollTop - st) <= delta) return;
+
+        if (sct > lastScrollTop && (lastScrollTop > 0)) {
+            $('.header').addClass('on')
+        } else {
+            $('.header').removeClass('on')
+        };
+        lastScrollTop = sct;
+
+        if (sct > 100) {
+            $('.header').addClass('bg')
+        } else {
+            $('.header').removeClass('bg')
+        };
+    });
+
+    $('.scrolldown a').on('click', function (event) {
+        event.preventDefault();
+        $('html, body').animate({ scrollTop: $(this.hash).offset().top - 100 }, 1000);
+    })
+
+    // main visual : scroll down
+
+    $('.main_product_slide').on('init afterChange', function (event, slick, currentSlide) {
+        var current = $('.main_product_slide .slick-current');
+        current.addClass('on').siblings().removeClass('on');
+    });
+
+    $('.main_product_slide').slick({
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 2500,
+        fade: true,
+        pauseOnHover: false,
+        pauseOnFocus: false,
+    });
+
+    //main product slide
+
+    $('.mainTechnology ul li').mouseenter(function () {
+        $(this).addClass('on').siblings().removeClass('on');
+    });
+
+    $(window).on('scroll', function () {
+        var sct = $(this).scrollTop();
+        if (sct > 500) {
+            $('.toTop').addClass('on');
+        } else {
+            $('.toTop').removeClass('on');
+        }
+    })
+
+    $('.toTop').on('click', function () {
+        $('html, body').stop().animate({ scrollTop: 0 }, 600)
+    })
+
 })
